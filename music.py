bba9180d094d89ce0ac0e42481b02818b6620e9e
unknown <gustavooliveirarei35@gmail.com>
2022-03-22 15:59:22 -0300

    commit music

---
+++ music.py
@@ -0,0 +1,178 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from pygame import mixer
+import os
+
+janela = Tk()
+janela.title("")
+janela.geometry("460x255")
+janela.configure(background="#feffff")
+janela.resizable(width=FALSE, height=FALSE)
+
+# Configurando Interface
+
+left = Frame(janela, width=150, height=150, bg="#2e2d2c")
+left.grid(row=0, column=0, pady=1, padx=1, sticky=NSEW)
+
+right = Frame(janela, width=250, height=200, bg="#2e2d2c")
+right.grid(row=0, column=1, pady=1, padx=0, sticky=NSEW)
+
+underside = Frame(janela, width=404, height=100, bg="#2e2d2c")
+underside.grid(row=1, column=0, columnspan=3, pady=1, padx=0, sticky=NSEW)
+
+# Configurando o frame left
+
+img_1 = Image.open("icon_spotify.png")
+img_1 = img_1.resize((130, 130))
+img_1 = ImageTk.PhotoImage(img_1)
+
+l_logo = Label(left, height=130, image=img_1, compound=LEFT, padx=10, anchor="nw",
+               font="ivy 16 bold", bg="#2e2d2c", fg="#2e2d2c")
+l_logo.place(x=6, y=15)
+
+
+# func
+
+def play_music():
+    run = listbox.get(ACTIVE)
+    l_run["text"] = run
+    mixer.music.load(l_run)
+    mixer.music.play()
+
+
+def pause_music():
+    phrase = ["Música pausada..."]
+    for f in phrase:
+        l_run["text"] = f
+        mixer.music.pause()
+
+
+def resume_music():
+    run = listbox.get(ACTIVE)
+    l_run["text"] = run
+    mixer.music.unpause()
+
+
+def stop_music():
+    run = listbox.get(ACTIVE)
+    l_run["text"] = "Música..."
+    mixer.music.load(run)
+    mixer.music.stop()
+
+
+def next_music():
+    sound = l_run["text"]
+    index = songs.index(sound)
+
+    new_index = index + 1
+    playing = songs[new_index]
+
+    mixer.music.load(sound)
+    mixer.music.play()
+
+    listbox.delete(0, END)
+    show()
+
+    listbox.select_set(new_index)
+    listbox.config(selectmode=SINGLE)
+    l_run["text"] = playing
+
+
+def previous_music():
+    sound = l_run["text"]
+    index = songs.index(sound)
+
+    new_index = index - 1
+    sound = songs[new_index]
+
+    mixer.music.load(sound)
+    mixer.music.play()
+
+    listbox.delete(0, END)
+    show()
+
+    listbox.select_set(new_index)
+    listbox.config(selectmode=SINGLE)
+    l_run["text"] = sound
+
+
+music = ["Melhor Dia 7 - Sossego", "Poesia Acústica 12 - Pra Sempre", "Sigo sozinho"]
+# Configurando o frame right
+
+listbox = Listbox(right, width=41, height=10, selectmode=SINGLE, font="arial 9 bold",
+                  bg="#2e2d2c", fg="#feffff")
+listbox.grid(row=0, column=0)
+
+s = Scrollbar(right)
+s.grid(row=0, column=1, sticky=NSEW)
+listbox.config(yscrollcommand=s.set)
+s.config(command=listbox.yview)
+
+# Configurando o frame underside
+
+l_run = Label(underside, text="Música...", width=58, justify=LEFT,
+              anchor="nw", font="ivy 10", bg="#feffff", fg="#2e2d2c")
+l_run.place(x=0, y=1)
+
+img_2 = Image.open("icon_rewind.png")
+img_2 = img_2.resize((30, 30))
+img_2 = ImageTk.PhotoImage(img_2)
+rewind = Button(underside, command=previous_music, width=40, height=40, image=img_2,
+                font="ivy 10 bold", relief=RAISED, overrelief=RIDGE,
+                bg="#2e2d2c", fg="#feffff")
+rewind.place(x=38, y=35)
+
+img_3 = Image.open("icon_play.png")
+img_3 = img_3.resize((30, 30))
+img_3 = ImageTk.PhotoImage(img_3)
+play = Button(underside, command=play_music, width=40, height=40, image=img_3,
+              font="ivy 10 bold", relief=RAISED, overrelief=RIDGE,
+              bg="#2e2d2c", fg="#feffff")
+play.place(x=84, y=35)
+
+img_4 = Image.open("icon_next.png")
+img_4 = img_4.resize((30, 30))
+img_4 = ImageTk.PhotoImage(img_4)
+next = Button(underside, command=next_music, width=40, height=40, image=img_4,
+              font="ivy 10 bold", relief=RAISED, overrelief=RIDGE,
+              bg="#2e2d2c", fg="#feffff")
+next.place(x=130, y=35)
+
+img_5 = Image.open("icon_pause.png")
+img_5 = img_5.resize((30, 30))
+img_5 = ImageTk.PhotoImage(img_5)
+pause = Button(underside, command=pause_music, width=40, height=40, image=img_5,
+               font="ivy 10 bold", relief=RAISED, overrelief=RIDGE,
+               bg="#2e2d2c", fg="#feffff")
+pause.place(x=176, y=35)
+
+img_6 = Image.open("icon_resume.png")
+img_6 = img_6.resize((30, 30))
+img_6 = ImageTk.PhotoImage(img_6)
+resume = Button(underside, command=resume_music, width=40, height=40, image=img_6,
+                font="ivy 10 bold", relief=RAISED, overrelief=RIDGE,
+                bg="#2e2d2c", fg="#feffff")
+resume.place(x=222, y=35)
+
+img_7 = Image.open("icon_stop.png")
+img_7 = img_7.resize((30, 30))
+img_7 = ImageTk.PhotoImage(img_7)
+stop = Button(underside, command=stop_music, width=40, height=40, image=img_7,
+              font="ivy 10 bold", relief=RAISED, overrelief=RIDGE,
+              bg="#2e2d2c", fg="#feffff")
+stop.place(x=268, y=35)
+
+os.chdir(r"C:\Users\Gustavo\Downloads\icons_python\musicas_python")
+songs = os.listdir()
+
+
+def show():
+    for song in songs:
+        listbox.insert(END, song)
+
+
+show()
+
+mixer.init()
+
+janela.mainloop()
